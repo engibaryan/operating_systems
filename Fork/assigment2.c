f2465897c7f31317fb9bc9c3d52063ccdbaeb8f2
engibaryan <xachik.engibar@gmail.com>
2024-09-08 18:40:57 +0400

    assigment 2 done

--- Fork/assigment2.c
+++ Fork/assigment2.c
@@ -4,20 +4,32 @@
 #include <stdlib.h>
 #include <wait.h>
 int main() {
-    pid_t child = fork();
+    pid_t child1, child2;
 
-    if(child < 0) {
+    child1 = fork();
+
+    if(child1 < 0) {
         //fork failed
         exit(1);
     }
-    else if(child == 0) {
+    else if(child1 == 0) {
         //child process
          execl("/bin/ls", "ls", NULL);
-    } else {
+         exit(1);
+    } 
+    child2 = fork();
+
+    if(child2 < 0) {
+        perror("Fork failed");
+    }
+    else if(child2 == 0) {
+        execl("/bin/date", "date", NULL);
+        exit(1);
+    }
         //parent process
         printf("Parent process done\n");
    
-    }
+    
  
     return 0;
 }
