c3b601b909e592ff15888f0261277005be3134bf
engibaryan <xachik.engibar@gmail.com>
2024-09-13 16:37:15 +0400

    processes assigment1

---
+++ Hmw2/Processes/assigment1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main() {
+    pid_t child = fork(); // creates child process
+
+    if(child == 0) { // in the child process prints its PID
+        printf("child PID %d\n", getpid());
+    }
+    else{ // in the parent process prints its PID
+        printf("parent PID %d\n",getpid());
+    }
+    return 0;
+}
